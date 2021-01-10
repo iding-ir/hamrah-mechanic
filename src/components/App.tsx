b8236a26d732e6525a768eddeeb2a29404b61710
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-01-10 20:46:01 +0330

    Parse string

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const inputEl = useRef(null);
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const value = event.currentTarget.value;
+    const parts = value.split("/");
+
+    const rows = parts.map((part) => {
+      let row: JSX.Element[] = [];
+
+      for (let i = 0; i < parseInt(part); i++) {
+        row = [...row, <div />];
+      }
+
+      return row;
+    });
+  };
+
   return (
     <div className="App">
-
+      <input ref={inputEl} onChange={onChange} />
     </div>
   );
 }
