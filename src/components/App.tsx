25a051ddf6b6cb20b88cfe1ef1d9426057906813
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-01-10 21:34:35 +0330

    Render items in DOM

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,38 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const inputEl = useRef(null);
 
+  const [rows, setRows] = useState<JSX.Element[][]>([]);
+
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
-    const parts = value.split("/");
+    
+    const counts = value.split("/");
 
-    const rows = parts.map((part) => {
+    let tempRow: JSX.Element[][]= [];
+
+    const divRows = counts.map((part) => {
       let row: JSX.Element[] = [];
 
       for (let i = 0; i < parseInt(part); i++) {
-        row = [...row, <div />];
+        row = [...row, <div className="Item" />];
       }
 
-      return row;
+      return <div className="Row">{row}</div>;
     });
+
+    tempRow = [...tempRow, divRows];
+
+    setRows(tempRow)
   };
 
   return (
     <div className="App">
       <input ref={inputEl} onChange={onChange} />
+
+      <div className="Wrapper">{rows}</div>
     </div>
   );
 }
